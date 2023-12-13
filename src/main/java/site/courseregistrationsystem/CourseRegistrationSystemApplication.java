d8e126d4f0450fe74471a58ad7694a9950bb3459
Hyun <ghkdgus29@naver.com>
2023-12-13 21:30:41 +0900

    chore: 스프링 프로젝트 환경 구축

---
+++ src/main/java/site/courseregistrationsystem/CourseRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package site.courseregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/courseregistrationsystem/CourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package site.courseregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
