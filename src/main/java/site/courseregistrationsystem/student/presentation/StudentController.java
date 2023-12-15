7c3bc90bc4d8e08ef9b97af59f84efd47573e0b3
yeonise <cerealnum@gmail.com>
2023-12-16 00:26:10 +0900

    modify: [presentation] CustomSession 대신 HttpSession을 사용하도록 변경

--- src/main/java/site/courseregistrationsystem/student/presentation/StudentController.java
+++ src/main/java/site/courseregistrationsystem/student/presentation/StudentController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import site.courseregistrationsystem.student.Student;
 import site.courseregistrationsystem.student.application.StudentService;
 import site.courseregistrationsystem.student.dto.LoginForm;
-import site.courseregistrationsystem.student.dto.StudentSession;
 import site.courseregistrationsystem.util.api.ApiResponse;
 import site.courseregistrationsystem.util.api.ResponseMessage;
 
@@ -18,13 +20,20 @@ import site.courseregistrationsystem.util.api.ResponseMessage;
 @RequiredArgsConstructor
 public class StudentController {
 
+	private static final String SESSION_KEY = "studentPK";
+
 	private final StudentService studentService;
 
-	@PostMapping
-	public ApiResponse<StudentSession> login(@RequestBody LoginForm loginForm) {
-		StudentSession studentSession = studentService.login(loginForm);
+	@PostMapping("/login")
+	public ApiResponse<Void> login(@RequestBody LoginForm loginForm, HttpServletRequest request) {
+		Student student = studentService.login(loginForm);  // 로그인 데이터 검증
 
-		return ApiResponse.of(HttpStatus.CREATED, ResponseMessage.LOGIN_SUCCESS.getMessage(), studentSession);
+		request.getSession().invalidate();  // 세션 파기
+		HttpSession session = request.getSession();  // 세션 생성
+		session.setAttribute(SESSION_KEY, student.getId());  // 세션에 studentPK 설정
+		session.setMaxInactiveInterval(3600);  // 60분동안 세션 유지
+
+		return ApiResponse.of(HttpStatus.CREATED, ResponseMessage.LOGIN_SUCCESS.getMessage(), null);
 	}
 
 }
