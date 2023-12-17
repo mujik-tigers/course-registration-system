eb7564be6d497a3ab9bcdb97ca8624211ad3a9bd
yeonise <cerealnum@gmail.com>
2023-12-17 12:35:15 +0900

    move: RedisConfig 패키지 이동

--- src/main/java/site/courseregistrationsystem/config/RedisConfig.java
+++ src/main/java/site/courseregistrationsystem/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package site.courseregistrationsystem.config.redis;
+package site.courseregistrationsystem.config;
 
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
