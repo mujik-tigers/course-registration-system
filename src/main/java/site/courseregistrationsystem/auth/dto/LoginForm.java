f7509248df995eb570afb33760ddf51a377ccf7a
yeonise <cerealnum@gmail.com>
2023-12-17 13:23:48 +0900

    modify: AllArgsConstructor 추가

--- src/main/java/site/courseregistrationsystem/auth/dto/LoginForm.java
+++ src/main/java/site/courseregistrationsystem/auth/dto/LoginForm.java
@@ -1,9 +1,13 @@
 package site.courseregistrationsystem.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import site.courseregistrationsystem.validator.PasswordFormatCheck;
 import site.courseregistrationsystem.validator.StudentIdFormatCheck;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class LoginForm {
 
