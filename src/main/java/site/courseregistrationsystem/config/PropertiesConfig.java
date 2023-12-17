287cdf11bdfc59e1f4c2ff5eb6cf801f0e699899
yeonise <cerealnum@gmail.com>
2023-12-17 14:37:08 +0900

    feat: EnableConfigurationProperties에 CookieProperties 등록

--- src/main/java/site/courseregistrationsystem/config/PropertiesConfig.java
+++ src/main/java/site/courseregistrationsystem/config/PropertiesConfig.java
@@ -3,11 +3,13 @@ package site.courseregistrationsystem.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import site.courseregistrationsystem.auth.presentation.CookieProperties;
 import site.courseregistrationsystem.util.encryption.Aes256Properties;
 
 @Configuration
 @EnableConfigurationProperties(value = {
-	Aes256Properties.class
+	Aes256Properties.class,
+	CookieProperties.class
 })
 public class PropertiesConfig {
 
