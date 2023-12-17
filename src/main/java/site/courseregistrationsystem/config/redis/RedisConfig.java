7fe8b6f6d77a45654bcc5c6b724910fbdc22cb22
yeonise <cerealnum@gmail.com>
2023-12-17 11:55:54 +0900

    feat: [config] RedisConfig 생성

---
+++ src/main/java/site/courseregistrationsystem/config/redis/RedisConfig.java
@@ -0,0 +1,34 @@
+package site.courseregistrationsystem.config.redis;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import lombok.RequiredArgsConstructor;
+import site.courseregistrationsystem.auth.StudentSession;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+	private final RedisProperties redisProperties;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+	}
+
+	@Bean
+	public RedisTemplate<String, StudentSession> redisTemplate() {
+		RedisTemplate<String, StudentSession> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		return redisTemplate;
+	}
+
+}
