0735c5e977850ba5356c0e6c90fd64e7c51bd7e5
yeonise <cerealnum@gmail.com>
2023-12-17 09:02:22 +0900

    feat: SessionManager 생성

---
+++ src/main/java/site/courseregistrationsystem/auth/util/SessionManager.java
@@ -0,0 +1,18 @@
+package site.courseregistrationsystem.auth.util;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import site.courseregistrationsystem.auth.StudentSession;
+
+@Component
+public class SessionManager {
+
+	public StudentSession generate(Long studentPk) {
+		String sessionId = UUID.randomUUID().toString();
+
+		return new StudentSession(sessionId, studentPk);
+	}
+
+}
