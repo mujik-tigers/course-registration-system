f0f2207df3f010d6e9e75a09cd46d8febc93ecaf
yeonise <cerealnum@gmail.com>
2023-12-17 13:15:37 +0900

    test: AuthController 테스트를 위한 MockBean 설정

--- src/test/java/site/courseregistrationsystem/RestDocsSupport.java
+++ src/test/java/site/courseregistrationsystem/RestDocsSupport.java
@@ -4,11 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@WebMvcTest(controllers = {})
+import site.courseregistrationsystem.auth.application.AuthService;
+import site.courseregistrationsystem.auth.application.SessionManager;
+import site.courseregistrationsystem.auth.presentation.AuthController;
+import site.courseregistrationsystem.util.encryption.Aes256Manager;
+
+@WebMvcTest(controllers = {AuthController.class})
 @AutoConfigureRestDocs
 public abstract class RestDocsSupport {
 
@@ -17,6 +23,15 @@ public abstract class RestDocsSupport {
 
 	@Autowired
 	protected ObjectMapper objectMapper;
+
+	@MockBean
+	protected AuthService authService;
+
+	@MockBean
+	protected Aes256Manager aes256Manager;
+
+	@MockBean
+	protected SessionManager sessionManager;
 
 	@BeforeEach
 	void setUp() {
