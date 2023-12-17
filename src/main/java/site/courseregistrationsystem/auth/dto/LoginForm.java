7010ea4c205ba96ded85301d42923367155a0443
yeonise <cerealnum@gmail.com>
2023-12-17 12:15:12 +0900

    feat: LoginForm DTO에 형식 검증 애너테이션 적용

--- src/main/java/site/courseregistrationsystem/auth/dto/LoginForm.java
+++ src/main/java/site/courseregistrationsystem/auth/dto/LoginForm.java
@@ -1,11 +1,16 @@
 package site.courseregistrationsystem.auth.dto;
 
 import lombok.Getter;
+import site.courseregistrationsystem.validator.PasswordFormatCheck;
+import site.courseregistrationsystem.validator.StudentIdFormatCheck;
 
 @Getter
 public class LoginForm {
 
+	@StudentIdFormatCheck
 	private String studentId;
+
+	@PasswordFormatCheck
 	private String password;
 
 }
