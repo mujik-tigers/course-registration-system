fc34bbc82773e6fe417718d29ccc1ffdb94ea6d6
Hyun <ghkdgus29@naver.com>
2023-12-19 19:56:50 +0900

    test: 세션 쿠키 여부를 검증하도록 RestDocsSupport 수정

--- src/test/java/site/courseregistrationsystem/clock/presentation/ClockControllerTest.java
+++ src/test/java/site/courseregistrationsystem/clock/presentation/ClockControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import jakarta.servlet.http.Cookie;
 import site.courseregistrationsystem.RestDocsSupport;
 
 class ClockControllerTest extends RestDocsSupport {
@@ -18,8 +19,14 @@ class ClockControllerTest extends RestDocsSupport {
 	@Test
 	@DisplayName("현재 서버시간 조회 : 성공")
 	void checkCurrentServerTime() throws Exception {
+		// given
+		String COOKIE_NAME = "SESSIONID";
+		String COOKIE_VALUE = "03166dc4-2c82-4e55-85f5-f47919f367a6";
+		Cookie sessionCookie = new Cookie(COOKIE_NAME, COOKIE_VALUE);
+
 		// when & then
-		mockMvc.perform(get("/clock/server"))
+		mockMvc.perform(get("/clock/server")
+				.cookie(sessionCookie))
 			.andDo(print())
 			.andExpect(status().isOk())
 			.andDo(document("current-server-time-fetch-success",
@@ -31,6 +38,25 @@ class ClockControllerTest extends RestDocsSupport {
 					fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
 					fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
 					fieldWithPath("data.currentServerTime").type(JsonFieldType.STRING).description("날짜 및 시간")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("현재 서버시간 조회 : 실패")
+	void checkFailCurrentServerTime() throws Exception {
+		// when & then
+		mockMvc.perform(get("/clock/server"))
+			.andDo(print())
+			.andExpect(status().isUnauthorized())
+			.andDo(document("current-server-time-fetch-fail",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				responseFields(
+					fieldWithPath("code").type(JsonFieldType.NUMBER).description("코드"),
+					fieldWithPath("status").type(JsonFieldType.STRING).description("상태"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
+					fieldWithPath("data").type(JsonFieldType.NULL).description("응답 데이터")
 				)
 			));
 	}
