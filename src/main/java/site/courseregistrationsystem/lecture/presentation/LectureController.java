c3dfe48615b78310528ea952d60ffd025ad872d5
yeonise <cerealnum@gmail.com>
2023-12-21 15:30:04 +0900

    feat: [presentation] LectureController 생성
    end point
    - GET /lectures : 강의 시간표 조회

---
+++ src/main/java/site/courseregistrationsystem/lecture/presentation/LectureController.java
@@ -0,0 +1,31 @@
+package site.courseregistrationsystem.lecture.presentation;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.courseregistrationsystem.lecture.application.LectureService;
+import site.courseregistrationsystem.lecture.dto.LectureDetail;
+import site.courseregistrationsystem.subject.SubjectDivision;
+import site.courseregistrationsystem.util.api.ApiResponse;
+import site.courseregistrationsystem.util.api.ResponseMessage;
+
+@RestController
+@RequiredArgsConstructor
+public class LectureController {
+
+	private final LectureService lectureService;
+
+	@GetMapping("/lectures")
+	public ApiResponse<Page<LectureDetail>> fetch(Pageable pageable,
+		@RequestParam(required = false) SubjectDivision subjectDivision,
+		@RequestParam(required = false) Long departmentId,
+		@RequestParam(required = false) String subjectName) {
+		return ApiResponse.ok(ResponseMessage.LECTURE_LIST_FETCH_SUCCESS.getMessage(),
+			lectureService.fetch(pageable, subjectDivision, departmentId, subjectName));
+	}
+
+}
