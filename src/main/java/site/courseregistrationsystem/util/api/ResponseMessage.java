b06df6d8bf0a15d65d4b95e23bd0667d8165a346
yeonise <cerealnum@gmail.com>
2023-12-21 15:22:05 +0900

    feat: 강의 목록 조회 성공 메시지 생성

--- src/main/java/site/courseregistrationsystem/util/api/ResponseMessage.java
+++ src/main/java/site/courseregistrationsystem/util/api/ResponseMessage.java
@@ -2,8 +2,11 @@ package site.courseregistrationsystem.util.api;
 
 public enum ResponseMessage {
 
-	// Student
-	LOGIN_SUCCESS("로그인에 성공했습니다");
+	// Auth
+	LOGIN_SUCCESS("로그인에 성공했습니다"),
+
+	// Lecture
+	LECTURE_LIST_FETCH_SUCCESS("강의 목록 조회에 성공했습니다");
 
 	private final String message;
 
