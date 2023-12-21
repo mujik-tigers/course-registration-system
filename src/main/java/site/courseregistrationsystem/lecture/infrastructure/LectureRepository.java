3246526d7ea4da002188ee4b77905f28652ba916
yeonise <cerealnum@gmail.com>
2023-12-21 15:27:28 +0900

    feat: [infrastructure] LectureRepository 생성
    - findMatchedLectures : 검색 조건(QueryParameter)에 일치하는 강의들을 조회합니다.

---
+++ src/main/java/site/courseregistrationsystem/lecture/infrastructure/LectureRepository.java
@@ -0,0 +1,25 @@
+package site.courseregistrationsystem.lecture.infrastructure;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import site.courseregistrationsystem.lecture.Lecture;
+import site.courseregistrationsystem.subject.SubjectDivision;
+
+public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+	@Query(value =
+		"SELECT l FROM Lecture l "
+			+ "JOIN FETCH l.subject s "
+			+ "JOIN FETCH s.department d "
+			+ "JOIN FETCH l.professor "
+			+ "WHERE (:subjectDivision IS NULL OR s.subjectDivision = :subjectDivision) "
+			+ "AND (:departmentId IS NULL OR d.id = :departmentId) "
+			+ "AND (:subjectName IS NULL OR s.name = :subjectName) "
+	)
+	Page<Lecture> findMatchedLectures(Pageable pageable, SubjectDivision subjectDivision, Long departmentId,
+		String subjectName);
+
+}
