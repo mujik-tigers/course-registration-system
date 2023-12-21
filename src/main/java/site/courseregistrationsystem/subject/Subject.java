f550c615217f09b298ed8e8894b1e7ac2d2e2dd9
yeonise <cerealnum@gmail.com>
2023-12-21 15:22:37 +0900

    modify: [domain] Subject - Getter 추가 및 연관 관계 생성

--- src/main/java/site/courseregistrationsystem/subject/Subject.java
+++ src/main/java/site/courseregistrationsystem/subject/Subject.java
@@ -3,17 +3,25 @@ package site.courseregistrationsystem.subject;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import site.courseregistrationsystem.department.Department;
 import site.courseregistrationsystem.student.Grade;
 
 @Entity
+@Getter
 public class Subject {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Department department;
 
 	@Enumerated(EnumType.STRING)
 	private SubjectDivision subjectDivision;
@@ -24,5 +32,5 @@ public class Subject {
 	private String name;
 	private Integer hoursPerWeek;
 	private Integer credits;
-	
+
 }
