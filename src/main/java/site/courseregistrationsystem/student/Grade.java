176dc03ee45edaf8eda4de04482fd99e895991e8
yeonise <cerealnum@gmail.com>
2023-12-21 15:21:03 +0900

    modify: Grade enum - number 필드 추가

--- src/main/java/site/courseregistrationsystem/student/Grade.java
+++ src/main/java/site/courseregistrationsystem/student/Grade.java
@@ -1,7 +1,16 @@
 package site.courseregistrationsystem.student;
 
+import lombok.Getter;
+
+@Getter
 public enum Grade {
 
-	FRESHMAN, SOPHOMORE, JUNIOR, SENIOR
+	FRESHMAN(1), SOPHOMORE(2), JUNIOR(3), SENIOR(4);
+
+	private final int number;
+
+	Grade(int number) {
+		this.number = number;
+	}
 
 }
