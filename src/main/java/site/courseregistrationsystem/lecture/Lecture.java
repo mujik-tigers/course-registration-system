6ff2f198d55c40f7ba398850389aa45cfa5fce04
yeonise <cerealnum@gmail.com>
2023-12-22 17:19:43 +0900

    modify: [domain] Lecture가 Schedule과의 연관 관계를 갖도록 변경

--- src/main/java/site/courseregistrationsystem/lecture/Lecture.java
+++ src/main/java/site/courseregistrationsystem/lecture/Lecture.java
@@ -1,6 +1,8 @@
 package site.courseregistrationsystem.lecture;
 
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,8 +10,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import site.courseregistrationsystem.professor.Professor;
+import site.courseregistrationsystem.schedule.Schedule;
 import site.courseregistrationsystem.subject.Subject;
 
 @Entity
@@ -22,14 +26,26 @@ public class Lecture {
 
 	private Integer lectureNumber;
 	private String lectureRoom;
-	private LocalTime lectureStartTime;
-	private LocalTime lectureEndTime;
 	private Integer totalCapacity;
+
+	@OneToMany(mappedBy = "lecture")
+	private List<Schedule> schedules = new ArrayList<>();
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Subject subject;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Professor professor;
+
+	public String generateSchedule() {
+		return schedules.stream().map(schedule ->
+				schedule.getDayOfWeek().getDescription()
+					+ "("
+					+ schedule.getFirstPeriod().getPeriodNumber()
+					+ "-"
+					+ schedule.getLastPeriod().getPeriodNumber()
+					+ ")")
+			.collect(Collectors.joining(", "));
+	}
 
 }
