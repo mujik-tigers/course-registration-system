1036868639a2bd32818c4ff44fba3d11dcf36fbd
yeonise <cerealnum@gmail.com>
2023-12-22 19:33:14 +0900

    modify: [presentation] pageable 기본 설정 추가

--- src/main/java/site/courseregistrationsystem/lecture/presentation/LectureController.java
+++ src/main/java/site/courseregistrationsystem/lecture/presentation/LectureController.java
@@ -1,13 +1,14 @@
 package site.courseregistrationsystem.lecture.presentation;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.courseregistrationsystem.lecture.application.LectureService;
-import site.courseregistrationsystem.lecture.dto.LectureDetailPage;
+import site.courseregistrationsystem.lecture.dto.LectureSchedulePage;
 import site.courseregistrationsystem.subject.SubjectDivision;
 import site.courseregistrationsystem.util.api.ApiResponse;
 import site.courseregistrationsystem.util.api.ResponseMessage;
@@ -19,7 +20,8 @@ public class LectureController {
 	private final LectureService lectureService;
 
 	@GetMapping("/lectures")
-	public ApiResponse<LectureDetailPage> fetch(Pageable pageable,
+	public ApiResponse<LectureSchedulePage> fetch(
+		@PageableDefault(size = 20, sort = "id") Pageable pageable,
 		@RequestParam(required = false) SubjectDivision subjectDivision,
 		@RequestParam(required = false) Long departmentId,
 		@RequestParam(required = false) String subjectName) {
