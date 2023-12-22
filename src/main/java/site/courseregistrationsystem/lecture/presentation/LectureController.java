28d6ccd9a028d7bbedda01770c0dceeb86399bfb
yeonise <cerealnum@gmail.com>
2023-12-22 19:00:15 +0900

    modify: [presentation] Page 객체 대신 DTO를 응답하도록 변경

--- src/main/java/site/courseregistrationsystem/lecture/presentation/LectureController.java
+++ src/main/java/site/courseregistrationsystem/lecture/presentation/LectureController.java
@@ -1,6 +1,5 @@
 package site.courseregistrationsystem.lecture.presentation;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.courseregistrationsystem.lecture.application.LectureService;
-import site.courseregistrationsystem.lecture.dto.LectureDetail;
+import site.courseregistrationsystem.lecture.dto.LectureDetailPage;
 import site.courseregistrationsystem.subject.SubjectDivision;
 import site.courseregistrationsystem.util.api.ApiResponse;
 import site.courseregistrationsystem.util.api.ResponseMessage;
@@ -20,7 +19,7 @@ public class LectureController {
 	private final LectureService lectureService;
 
 	@GetMapping("/lectures")
-	public ApiResponse<Page<LectureDetail>> fetch(Pageable pageable,
+	public ApiResponse<LectureDetailPage> fetch(Pageable pageable,
 		@RequestParam(required = false) SubjectDivision subjectDivision,
 		@RequestParam(required = false) Long departmentId,
 		@RequestParam(required = false) String subjectName) {
