109fd1bf85da6629f6921a8b0c0b56dd39ffc8a0
yeonise <cerealnum@gmail.com>
2023-12-23 19:25:32 +0900

    test: DisplayName 추가

--- src/test/java/site/courseregistrationsystem/auth/application/SessionManagerTest.java
+++ src/test/java/site/courseregistrationsystem/auth/application/SessionManagerTest.java
@@ -3,6 +3,7 @@ package site.courseregistrationsystem.auth.application;
 import static org.assertj.core.api.Assertions.*;
 
 import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,6 +18,7 @@ class SessionManagerTest extends IntegrationTestSupport {
 	private SessionManager sessionManager;
 
 	@Test
+	@DisplayName("세션 생성 시 길이 36의 세션 ID, 학생 PK, 3600의 만료 시간을 갖는다")
 	void generate() {
 		// given
 		Long studentPk = 100L;
@@ -31,6 +33,7 @@ class SessionManagerTest extends IntegrationTestSupport {
 	}
 
 	@Test
+	@DisplayName("세션 ID로 데이터베이스에서 세션을 조회한다")
 	void fetch() {
 		// given
 		Long studentPk = 100L;
@@ -45,6 +48,7 @@ class SessionManagerTest extends IntegrationTestSupport {
 	}
 
 	@Test
+	@DisplayName("세션 ID와 일치하는 세션을 데이터베이스에서 삭제한다")
 	void invalidate() {
 		// given
 		Long studentPk = 100L;
@@ -60,6 +64,7 @@ class SessionManagerTest extends IntegrationTestSupport {
 	}
 
 	@Test
+	@DisplayName("기존의 세션은 무효화하고 동일한 학생 PK를 갖는 새로운 세션을 생성한다")
 	void renew() {
 		// given
 		Long studentPk = 100L;
@@ -71,6 +76,9 @@ class SessionManagerTest extends IntegrationTestSupport {
 		// then
 		assertThat(renewed.getId()).isNotEqualTo(session.getId());
 		assertThat(renewed.getStudentPk()).isEqualTo(session.getStudentPk());
+		assertThatThrownBy(() -> sessionManager.fetch(session.getId()))
+			.isInstanceOf(NonexistenceSessionException.class)
+			.hasMessage(ErrorType.NONEXISTENT_SESSION.getMessage());
 	}
 
 }
