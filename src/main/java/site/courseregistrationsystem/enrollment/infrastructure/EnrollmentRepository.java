17b6c537aecd352e4fafc79b165d6a8f5c10002a
yeonise <cerealnum@gmail.com>
2024-01-02 20:45:25 +0900

    feat: [infrastructure] 학생의 해당 학기 내 수강 신청 내역을 조회하는 쿼리 생성

--- src/main/java/site/courseregistrationsystem/enrollment/infrastructure/EnrollmentRepository.java
+++ src/main/java/site/courseregistrationsystem/enrollment/infrastructure/EnrollmentRepository.java
@@ -1,9 +1,24 @@
 package site.courseregistrationsystem.enrollment.infrastructure;
 
+import java.time.Year;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import site.courseregistrationsystem.enrollment.Enrollment;
+import site.courseregistrationsystem.lecture.Semester;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
+	@Query("SELECT e FROM Enrollment e "
+		+ "JOIN e.student s "
+		+ "JOIN FETCH e.lecture l "
+		+ "JOIN FETCH l.schedules "
+		+ "JOIN FETCH l.subject ls "
+		+ "WHERE s.id = :studentPk "
+		+ "AND l.openingYear = :openingYear "
+		+ "AND l.semester = :semester")
+	List<Enrollment> findAllInCurrentSemester(Long studentPk, Year openingYear, Semester semester);
 
 }
