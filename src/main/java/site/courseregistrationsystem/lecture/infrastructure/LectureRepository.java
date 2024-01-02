3cca4a8106b86096cab2975e6292e580049eeb44
yeonise <cerealnum@gmail.com>
2024-01-02 20:44:47 +0900

    feat: [infrastructure] Lecture와 Schedule을 함께 fetch하는 쿼리 생성

--- src/main/java/site/courseregistrationsystem/lecture/infrastructure/LectureRepository.java
+++ src/main/java/site/courseregistrationsystem/lecture/infrastructure/LectureRepository.java
@@ -1,9 +1,17 @@
 package site.courseregistrationsystem.lecture.infrastructure;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import site.courseregistrationsystem.lecture.Lecture;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long>, LectureRepositoryCustom {
+
+	@Query("SELECT l FROM Lecture l "
+		+ "JOIN FETCH l.schedules "
+		+ "WHERE l.id = :lectureId")
+	Optional<Lecture> findWithSchedule(Long lectureId);
 
 }
