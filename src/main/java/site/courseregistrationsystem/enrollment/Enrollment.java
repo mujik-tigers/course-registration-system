69e31f6a616b75b6624f71c9d783db42e4b10a69
yeonise <cerealnum@gmail.com>
2024-01-02 17:30:40 +0900

    feat: Builder를 사용하여 생성자 구현

--- src/main/java/site/courseregistrationsystem/enrollment/Enrollment.java
+++ src/main/java/site/courseregistrationsystem/enrollment/Enrollment.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import site.courseregistrationsystem.lecture.Lecture;
 import site.courseregistrationsystem.student.Student;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Enrollment {
 
@@ -23,5 +27,15 @@ public class Enrollment {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Lecture lecture;
+
+	@Builder
+	private Enrollment(Student student, Lecture lecture) {
+		this.student = student;
+		this.lecture = lecture;
+	}
+
+	public Long fetchLectureId() {
+		return lecture.getId();
+	}
 
 }
