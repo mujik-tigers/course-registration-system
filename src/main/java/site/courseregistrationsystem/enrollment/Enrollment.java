909450121939bc164a36f3239a31759c3519971d
yeonise <cerealnum@gmail.com>
2024-01-02 13:57:24 +0900

    feat: [domain] Enrollment Entity 생성

---
+++ src/main/java/site/courseregistrationsystem/enrollment/Enrollment.java
@@ -0,0 +1,27 @@
+package site.courseregistrationsystem.enrollment;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import site.courseregistrationsystem.lecture.Lecture;
+import site.courseregistrationsystem.student.Student;
+
+@Entity
+@Getter
+public class Enrollment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Student student;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Lecture lecture;
+
+}
