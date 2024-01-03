d673d17a0b6b55b743caf864854350fcd4ecfd54
yeonise <cerealnum@gmail.com>
2024-01-03 13:38:27 +0900

    modify: 학점을 초과하여 수강 신청을 시도하는 경우 발생하는 예외 이름 변경

--- src/main/java/site/courseregistrationsystem/exception/enrollment/CreditsLimitExceededException.java
+++ src/main/java/site/courseregistrationsystem/exception/enrollment/CreditsLimitExceededException.java
@@ -3,9 +3,9 @@ package site.courseregistrationsystem.exception.enrollment;
 import site.courseregistrationsystem.exception.CustomException;
 import site.courseregistrationsystem.exception.ErrorType;
 
-public class EnrollmentLimitExceededException extends CustomException {
+public class CreditsLimitExceededException extends CustomException {
 
-	public EnrollmentLimitExceededException() {
+	public CreditsLimitExceededException() {
 		super(ErrorType.SEMESTER_CREDIT_EXCEED);
 	}
 
