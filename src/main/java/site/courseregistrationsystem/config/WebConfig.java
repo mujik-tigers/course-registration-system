d47b96b49eb4a74c4cbb7f83af2dd855ae63625a
yeonise <cerealnum@gmail.com>
2024-01-17 15:00:05 +0900

    modify: [config] 개발 서버 기준 allowedOrigins 설정

--- src/main/java/site/courseregistrationsystem/config/WebConfig.java
+++ src/main/java/site/courseregistrationsystem/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowCredentials(true)
-			.allowedOrigins("*")
+			.allowedOrigins("http://localhost:8081")
 			.allowedMethods(
 				HttpMethod.HEAD.name(), HttpMethod.GET.name(), HttpMethod.POST.name(),
 				HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(),
