f1a3c317b0f6722acb63b7f6cc9fd26d2c72bed2
yeonise <cerealnum@gmail.com>
2024-01-17 14:18:12 +0900

    modify: [config] 쿠키를 주고 받을 수 있도록 allowCredentials true로 변경

--- src/main/java/site/courseregistrationsystem/config/WebConfig.java
+++ src/main/java/site/courseregistrationsystem/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
+			.allowCredentials(true)
 			.allowedOrigins("*")
 			.allowedMethods(
 				HttpMethod.HEAD.name(), HttpMethod.GET.name(), HttpMethod.POST.name(),
