895fdb1112152b76a2a8d4e2a5597845235d778a
yeonise <cerealnum@gmail.com>
2024-02-27 16:38:37 +0900

    config: RedisCacheManager 등록
    강의의 신청 인원 수를 조회할 때 사용할 캐시 매니저를 등록합니다.

--- src/main/java/site/courseregistrationsystem/config/RedisConfig.java
+++ src/main/java/site/courseregistrationsystem/config/RedisConfig.java
@@ -1,17 +1,26 @@
 package site.courseregistrationsystem.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableCaching
 @RequiredArgsConstructor
 public class RedisConfig {
 
@@ -33,6 +42,19 @@ public class RedisConfig {
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 
 		return redisTemplate;
+	}
+
+	@Bean
+	public CacheManager applicantsCacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+			.entryTtl(Duration.ofMinutes(30));
+
+		return RedisCacheManager
+			.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
+			.cacheDefaults(cacheConfiguration)
+			.build();
 	}
 
 }
